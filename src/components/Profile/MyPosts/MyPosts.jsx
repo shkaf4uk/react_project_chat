d7db38241e32f67c3e4be41891b67acf9f198401
shkaf4uk <shafarenkomaksym@gmail.com>
2020-11-22 19:03:30 +0200

    props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './MyPosts.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return (
         <div>
             <div>My posts</div>
             <div>New post</div>
             <textarea></textarea>
             <button>Add post</button>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post massage={'Hi, how are you?'} likes={'5'}/>
+            <Post massage={"It's my first post"} likes={20}/>
+            <Post likes={1}/>
             <Post/>
         </div>
     )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,9 @@ import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return (
         <div className={'content'}>
-            <div><img src={'https://img.theculturetrip.com/768x432/wp-content/uploads/2016/03/suspension-bridge-1149942_1280.jpg'} /></div>
+            <div><img
+                src={'https://img.theculturetrip.com/768x432/wp-content/uploads/2016/03/suspension-bridge-1149942_1280.jpg'}/>
+            </div>
             <div><img src={'https://i.pinimg.com/564x/7a/7e/be/7a7ebe744313282a2bfa30cb154a39cb.jpg'}/></div>
             <div>ava + description</div>
             <MyPosts/>
