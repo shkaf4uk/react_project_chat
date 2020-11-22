ddcf62f5d470e1b3703539382bff088c7f40e8b5
shkaf4uk <shafarenkomaksym@gmail.com>
2020-11-22 12:35:27 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+        <img src={'https://www.pinclipart.com/picdir/middle/51-511102_design-free-logo-srj-hd-logo-png-clipart.png'}/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div><img src={'https://img.theculturetrip.com/768x432/wp-content/uploads/2016/03/suspension-bridge-1149942_1280.jpg'} /></div>
+            <div><img src={'https://i.pinimg.com/564x/7a/7e/be/7a7ebe744313282a2bfa30cb154a39cb.jpg'}/></div>
+            <div>ava + description</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
+
+export default Profile;
