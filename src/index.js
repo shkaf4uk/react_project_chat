4df0aeadfff34369fc6b767c41564d9c3e9399ce
shkaf4uk <shafarenkomaksym@gmail.com>
2020-11-26 19:51:39 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {addPost, sendMessage, updateNewMessageText, updateNewPostText} from './redux/state';
 
 let rerenderEntriesTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <App state={state}
-                 addPost={addPost}
-                 updateNewPostText={updateNewPostText}
-                 sendMessage={sendMessage}
-                 updateNewMessageText={updateNewMessageText}/>
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 sendMessage={store.sendMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerenderEntriesTree(state);
+rerenderEntriesTree(store.getState());
 
-subscribe(rerenderEntriesTree);
+store.subscribe(rerenderEntriesTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,77 +1,77 @@
-let rerenderEntriesTree = () => {
-    console.log('State was change');
-}
-
-let state = {
-    profilePage: {
-        posts:[
-            {id: 1, massage: 'Hi, how are you?', likesCount: 5},
-            {id: 2, massage: "It's my first post", likesCount: 20},
-            {id: 2, massage: "TEST", likesCount: 2},
-            {id: 2, massage: "TES2", likesCount: 24},
-            {id: 3, likesCount: 1},
-        ],
-        newPostText: '',
+let store = {
+    _state: {
+        profilePage: {
+            posts:[
+                {id: 1, massage: 'Hi, how are you?', likesCount: 5},
+                {id: 2, massage: "It's my first post", likesCount: 20},
+                {id: 2, massage: "TEST", likesCount: 2},
+                {id: 2, massage: "TES2", likesCount: 24},
+                {id: 3, likesCount: 1},
+            ],
+            newPostText: '',
+        },
+        dialogPage: {
+            dialogs:[
+                {id: 1, name: 'Mike'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Nasty'},
+                {id: 4, name: 'Max'},
+                {id: 5, name: 'Alla'},
+                {id: 6, name: 'Nikolai'},
+            ],
+            massages:[
+                {id: 1, massage: 'Hi'},
+                {id: 2, massage: 'Hello'},
+                {id: 3, massage: 'Старт!'},
+                {id: 4, massage: 'Yo'},
+                {id: 5, massage: 'Yo'},
+                {id: 6, massage: 'Yo'},
+            ],
+            newMessage: '',
+        },
     },
-   dialogPage: {
-       dialogs:[
-           {id: 1, name: 'Mike'},
-           {id: 2, name: 'Andrey'},
-           {id: 3, name: 'Nasty'},
-           {id: 4, name: 'Max'},
-           {id: 5, name: 'Alla'},
-           {id: 6, name: 'Nikolai'},
-       ],
-       massages:[
-           {id: 1, massage: 'Hi'},
-           {id: 2, massage: 'Hello'},
-           {id: 3, massage: 'Старт!'},
-           {id: 4, massage: 'Yo'},
-           {id: 5, massage: 'Yo'},
-           {id: 6, massage: 'Yo'},
-       ],
-       newMessage: '',
-   },
+    _callSubscriber() {
+        console.log('State was change');
+    },
+    getState() {
+        return this._state
+    },
+    addPost () {
+        let newPost = {
+            id: 5,
+            massage: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newPost) {
+        this._state.profilePage.newPostText = newPost;
+        this._callSubscriber(this._state);
+    },
+    sendMessage() {
+        let newMessage = {
+            id: 5,
+            massage: this._state.dialogPage.newMessage,
+            likesCount: 0
+        };
+        this._state.dialogPage.massages.push(newMessage);
+        this._state.dialogPage.newMessage = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText (newMess) {
+        this._state.dialogPage.newMessage = newMess;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-window.state = state;
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        massage: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntriesTree(state);
-}
 
-export let updateNewPostText = (newPost) => {
-    state.profilePage.newPostText = newPost;
-    rerenderEntriesTree(state);
-}
 
-export let sendMessage = () => {
-    let newMessage = {
-        id: 5,
-        massage: state.dialogPage.newMessage,
-        likesCount: 0
-    };
-    state.dialogPage.massages.push(newMessage);
-    state.dialogPage.newMessage = '';
-    rerenderEntriesTree(state);
-}
-
-export let updateNewMessageText = (newMess) => {
-    state.dialogPage.newMessage = newMess;
-    rerenderEntriesTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntriesTree = observer;
-}
-
-export default state;
-
+export default store;
+window.stote = store;
 // store - OOP
